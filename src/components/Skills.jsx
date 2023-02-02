134efffd6d971b1a388b794b6cf6ff6a03e2ab77
carlosn97 <carlosnasayov@gmail.com>
2023-02-01 23:34:51 -0500

    skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,82 @@
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import '../styles.css/skilss.css'
 
-function BasicExample() {
+const Skills=()=> {
   return(
-    <p></p>
+    <>
+     <div className="section" id="skill">
+        <div className="container">
+          <div className="h4 text-center mb-4 title">Professional Skills</div>
+          <div className="card" data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="progress-container progress-primary"><span className="progress-badge">Javascript</span>
+                    <div className="progress">
+                      <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10"
+                        data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style={{width: '70%'}}></div><span className="progress-value"></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="progress-container progress-primary"><span className="progress-badge">Python</span>
+                    <div className="progress">
+                      <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10"
+                        data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style={{width: '70%'}}></div><span className="progress-value"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="progress-container progress-primary"><span className="progress-badge">React</span>
+                    <div className="progress">
+                      <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10"
+                        data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style={{width: '60%'}}></div><span className="progress-value"></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="progress-container progress-primary"><span className="progress-badge">Backend</span>
+                    <div className="progress">
+                      <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10"
+                        data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style={{width: '50%'}}></div><span className="progress-value"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="progress-container progress-primary"><span className="progress-badge">Frontend</span>
+                    <div className="progress">
+                      <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10"
+                        data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style={{width: '70%'}}></div><span className="progress-value"></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="progress-container progress-primary"><span className="progress-badge">Microsoft Office</span>
+                    <div className="progress">
+                      <div className="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10"
+                        data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style={{width: '60%'}}></div><span className="progress-value"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </>
+    
   ) 
 }
 
-export default BasicExample;
+export default Skills;
